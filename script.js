b285227393c02e38079fb35704dfc7f4a8968628
Deepak Kumar <40654292+deepak-bits@users.noreply.github.com>
2023-08-19 17:35:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
 
+
+const inp = document.getElementById("newTodoInput");
+const ol = document.getElementById("todoList");
+const btn = document.getElementById("addTodoBtn");
+
+btn.addEventListener('click', function() {
+	const l1 = document.createElement('li');
+	l1.innerText = inp.value;
+	ol.append(li);
+})
